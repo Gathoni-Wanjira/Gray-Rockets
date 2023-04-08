dfebb3f12d38ca0f1e789f52d6ecc9d1f5158084
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-04-08 20:30:10 +0300

    Tailwind config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./Build/*.html"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
 
+// The content inside [content] means that we want to run all files in the Build folder that are 
+// html files.
